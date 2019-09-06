3e6cce29e138a51a6f64d1b05cfbc49503567d89
Viniciusog <vinijipa23@gmail.com>
2019-09-05 21:20:20 -0400

    Configure icons and menu click

--- app/src/main/java/com/example/todolist/activity/AdicionarTarefaActivity.java
+++ app/src/main/java/com/example/todolist/activity/AdicionarTarefaActivity.java
@@ -1,8 +1,12 @@
 package com.example.todolist.activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.example.todolist.R;
 
@@ -12,5 +16,26 @@ public class AdicionarTarefaActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_adicionar_tarefa);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_adicionar_tarefa, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        switch ( item.getItemId() ){
+            case R.id.itemSalvar: {
+                //Executa a ação do botão de confirmar tarefa
+                Toast.makeText(AdicionarTarefaActivity.this,
+                        "Botão Confirmar Pressionado!",
+                        Toast.LENGTH_SHORT).show();
+            }
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
